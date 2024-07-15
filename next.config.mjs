3a1e1d3e9aa53bb59dde9c18acb3c45c061e724c
pierluigigiancola <pierluigi.giancola@gmail.com>
2024-07-15 22:26:32 +0200

    chore: allow twitter profile images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+        port: "",
+        pathname: "/profile_images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
